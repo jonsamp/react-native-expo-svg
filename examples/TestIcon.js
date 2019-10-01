d2f0f3fbf6da0810e79c81480dc547e3c9df115c
jonsamp <sampjon@gmail.com>
2019-09-30 23:51:18 -0400

    adds examples

---
+++ examples/TestIcon.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Path, Svg } from 'react-native-svg'
+
+function TestIcon() {
+return (
+  <Svg width={29} height={40} viewBox="0 0 29 40" fill="none"><Path d="M5.65 38.123h20.236" stroke="#000" strokeWidth="2" strokeLinecap="round"/><Path d="M6.77 38.124v-24.6h21.016s-2.928 2.488-2.928 4.2v20.4" stroke="#000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/><Path d="M6.77 17.612c-5.345 0-5.345.011-5.345 6.668s0 6.657 5.345 6.657" stroke="#000" strokeWidth="2"/><Path clipRule="evenodd" d="M24.443 10.902c0-4.299-3.884-7.783-8.675-7.783-4.79 0-8.674 3.484-8.674 7.783h17.349z" stroke="#000" strokeWidth="2" strokeLinejoin="round"/><Path fillRule="evenodd" clipRule="evenodd" d="M15.768 3.481a1.538 1.538 0 1 0 0-3.075 1.538 1.538 0 0 0 0 3.075z" fill="#000"/><Path d="M9.814 26.55h12M15.966 10.902v15.424" stroke="#000" strokeWidth="2" strokeLinecap="round"/></Svg>
+);
+}
+
+export default TestIcon;
